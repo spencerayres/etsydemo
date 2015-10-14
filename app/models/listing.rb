060be29670fd04a7fed1a0b0ea085fef438f268d
Spencer Ayres <spjayres@gmail.com>
2015-10-14 17:07:46 +0100

    added the file rename line

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -4,7 +4,8 @@ class Listing < ActiveRecord::Base
 	else
 		has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.jpg",
   		:storage => :dropbox,
-	    :dropbox_credentials => Rails.root.join("config/dropbox.yml")
+	    :dropbox_credentials => Rails.root.join("config/dropbox.yml"),
+	    :path => ":style/:id_:filename"
 	end
 	 	
 
