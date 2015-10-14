2e242b2fa21b84b0efc6c85bbc16db3e36e8ecc8
Spencer Ayres <spjayres@gmail.com>
2015-10-14 17:19:51 +0100

    still trying

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,12 +1,9 @@
 class Listing < ActiveRecord::Base
-	if Rails.env.development?
-		has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.jpg"
-	else
+	
 		has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.jpg",
   		:storage => :dropbox,
-	    :dropbox_credentials => Rails.root.join("config/dropbox.yml"),
-	    :path => ":style/:id_:filename"
-	end
+	    :dropbox_credentials => Rails.root.join("config/dropbox.yml")
+
 	 	
 
 end
